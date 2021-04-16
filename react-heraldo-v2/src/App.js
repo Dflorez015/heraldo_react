a37c907d71a74c009a8700c4762224d90b548530
Dflorez015 <duvanflorez013@gmail.com>
2021-04-16 07:16:07 -0500

    Seccion terminada

--- react-heraldo-v2/src/App.js
+++ react-heraldo-v2/src/App.js
@@ -1,12 +1,25 @@
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
+import CardsLeftSection from './components/CardsLeftSection.js';
+import CardsMidSection from './components/CardsMidSection.js';
+
 
 function App() {
   return (
     <div class="container">
       <Header />
+      <div className="row">
+        <div className="col-md-3 col-sm-12">
+          <CardsLeftSection />
+        </div>
+        <div className="col-md-6 col-sm-12">
+          <CardsMidSection />
+        </div>
+        <div className="col-md-3 col-sm-12">
+          <CardsLeftSection />
+        </div>
+      </div>
       <Footer />
-
     </div>
   );
 }
